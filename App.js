24927bdeea75bb8be081a2b2efbd4ed84c98626e
Jack C <jacktheclark@gmail.com>
2023-11-17 14:51:17 -0800

    1234

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>thirdddddd</Text>
+      <Text>second</Text>
       <StatusBar style="auto" />
     </View>
   );
