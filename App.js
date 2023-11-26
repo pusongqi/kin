61994d671d53c80319cf4bc55fb3e5b089e72c91
Steven Pu <pusongqi@gmail.com>
2023-11-25 20:33:41 -0500

    Added big card

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { BigCard } from "./components/BigCard";
 
 export default function App() {
+  const [data, setData] = useState("");
+
+  function handleTouchStart() {
+    setData("new data");
+  }
+
   return (
     <View style={styles.container}>
       <Text>thirdddddd</Text>
       <StatusBar style="auto" />
+      <View style={styles.bigCard} onTouchStart={handleTouchStart}>
+        <Text>{data}</Text>
+      </View>
     </View>
   );
 }
@@ -16,5 +27,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  bigCard: {
+    backgroundColor: "green",
+    width: 200,
+    height: 200,
+    borderRadius: 20,
   },
 });
